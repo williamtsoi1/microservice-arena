53946dcb4d02da419614c23e356006e954fe525b
William Tsoi <william@williamtsoi.net>
2022-02-24 13:55:14 +0800

    adding enemy detection on same axis

--- web.js
+++ web.js
@@ -31,9 +31,81 @@ app.get('/', function (req, res) {
 //   }
 
 app.post('/', function (req, res) {
-  console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  // console.log(req.body);
+  const self_url = req.body._links.self.href;
+  const arena_width = req.body.arena.dims[0];
+  const arena_height = req.body.arena.dims[1];
+  const self_state = req.body.arena.state[self_url];
+  var response_body = "";
+  
+  // check if there is someone 3 squares ahead of me
+  console.log("At " + self_state.x + "," + self_state.y)
+  var enemy_found = false;
+  var enemy_same_axis = false;
+  switch (self_state.direction) {
+    case "N":
+        // check if there are enemies with same x value, but smaller y value
+        console.log("Facing N")
+        for (var enemy_url in req.body.arena.state) {
+            if (enemy_url != self_url) {
+                var enemy_state = req.body.arena.state[enemy_url];
+                if ((enemy_state.x == self_state.x) && (enemy_state.y < self_state.y)) {
+                  console.log("Found near enemy: " + enemy_url + " " + enemy_state.x + "," + enemy_state.y + " in direction N");
+                  enemy_found = true;
+                }
+            }
+        }
+        break;
+    case "W":
+        // check if there are enemies with smaller x value, but same y value
+        console.log("Facing W")
+        for (var enemy_url in req.body.arena.state) {
+            if (enemy_url != self_url) {                  
+                var enemy_state = req.body.arena.state[enemy_url];
+                if ((enemy_state.x < self_state.x) && (enemy_state.y == self_state.y)) {
+                  console.log("Found enemy: " + enemy_url + " " + enemy_state.x + "," + enemy_state.y + " in direction W");
+                  enemy_found = true;
+                }
+            }
+        }
+        break;
+    case "E":
+        // check if there are enemies with larger x value, but same y value
+        console.log("Facing E")
+        for (var enemy_url in req.body.arena.state) {
+            if (enemy_url != self_url) {
+                var enemy_state = req.body.arena.state[enemy_url];
+                if ((enemy_state.x > self_state.x) && (enemy_state.y == self_state.y)) {
+                  console.log("Found enemy: " + enemy_url + " " + enemy_state.x + "," + enemy_state.y + " in direction E");
+                  enemy_found = true;
+                } 
+            }
+        }
+        break;
+    case "S":
+        // check if there are enemies with same x value, but larger y value
+        console.log("Facing S")
+        for (var enemy_url in req.body.arena.state) {
+            if (enemy_url != self_url) {
+                var enemy_state = req.body.arena.state[enemy_url];
+                if ((enemy_state.x == self_state.x) && (enemy_state.y - self_state.y)) {
+                  console.log("Found enemy: " + enemy_url + " " + enemy_state.x + "," + enemy_state.y + " in direction S");
+                  enemy_found = true;
+                }
+            }
+        }
+        break;
+  }
+  if (enemy_found) {
+      console.log("Throwing");
+      response_body = "T";
+  }
+  else {
+      console.log("No enemy found, make random move");
+      const moves = ['F', 'L', 'R'];
+      response_body = moves[Math.floor(Math.random() * moves.length)];
+  }
+  res.send(response_body);
 });
 
 app.listen(process.env.PORT || 8080);
